b79a544dccd3e57a62c87a183c4e154d0181aa3c
Diego Meneses <di.menesesv@duocuc.cl>
2024-11-06 17:42:40 -0300

    creación de product service

--- src/app/services/API/products.service.ts
+++ src/app/services/API/products.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
+import {ProductoRespuesta} from 'src/api/interfaces/ProductoRespuesta'
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private readonly URL = '';
+  private readonly URL = 'https://dummyjson.com/auth/products';
+  public productos: ProductoRespuesta;
+  constructor(
+    private http : HttpClient,
+    private auth : AuthService
 
-  constructor() { }
+  ) { }
 
-  public listarProductos(){}
+  public listarProductos(){
+    const requestheaders: string|null = this.auth.getToken()
+    this.http.get<ProductoRespuesta>(this.URL,{
+      headers:{
+        'Authorization' : 'Bearer' +requestheaders
+      }
+    }).subscribe(response =>{
+      response.
+    }
+    )
+  }
 
-  
+
 }
